8a8a25210e27400a0c985bbf3b7bdb562d06b18e
Nadezhda Babicheva <nadezhda.babicheva@egamings.com>
2021-08-16 09:03:49 +0300

    3rd task - 02, 03, 04

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+ if(size === 0) {
+   return '';
+ } else if (!size) {
+   return string;
+ }
+  const chars = string.split('');
+  let count = 1;
+  let res = chars[0] || '';
+  for (let i = 1; i < chars.length; i++) {
+    if (chars[i] === chars[i - 1]) {
+      count += 1;
+    } else {
+      count = 1;
+    }
+    if (count <= size) {
+      res += chars[i];
+    }
+  }
+  return res;
 }
